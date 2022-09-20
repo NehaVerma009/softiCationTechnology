69a42589e467ee9fe4ba340ae9aee5be4cfab926
Tejush Singh <tejush1998@gmail.com>
2022-09-20 13:49:21 +0530

    models done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 var bodyParser = require('body-parser');
-const multer = require('multer')
-
+// const multer = require('multer')
 const route = require('./routes/route')
 const app = express();
 const mongoose=require('mongoose')
-mongoose.connect("mongodb+srv://book-management:31VMJSSWSjcdoaAO@cluster0.eemzti8.mongodb.net/book-management?retryWrites=true&w=majority", {
+
+mongoose.connect("mongodb+srv://book-management:31VMJSSWSjcdoaAO@cluster0.eemzti8.mongodb.net/group48Database?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
@@ -13,12 +13,12 @@ mongoose.connect("mongodb+srv://book-management:31VMJSSWSjcdoaAO@cluster0.eemzti
 
 
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(multer().any())
+// app.use(multer().any()) 
 app.use('/', route);
-app.listen(process.env.PORT || 3001, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3001))
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
 
 
