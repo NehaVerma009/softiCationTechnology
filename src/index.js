fafe5cbe7b325a19541fc2dcd3eb5b54d64127be
Tejush Singh <tejush1998@gmail.com>
2022-09-20 12:34:31 +0530

    index

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+const multer = require('multer')
+
+const route = require('./routes/route')
+const app = express();
+const mongoose=require('mongoose')
+mongoose.connect("mongodb+srv://book-management:31VMJSSWSjcdoaAO@cluster0.eemzti8.mongodb.net/book-management?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer().any())
+app.use('/', route);
+app.listen(process.env.PORT || 3001, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3001))
+});
+
+
